1feceef1b8e2a8afa05f523a1ae46eddf1c15228
piomin <piotr.minkowski@gmail.com>
2026-03-11 12:20:54 +0100

    fix errors + lombok removal

--- person-service/src/main/java/pl/piomin/samples/spring/bluegreen/person/model/Person.java
+++ person-service/src/main/java/pl/piomin/samples/spring/bluegreen/person/model/Person.java
@@ -1,15 +1,9 @@
 package pl.piomin.samples.spring.bluegreen.person.model;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import jakarta.persistence.*;
 
 @Entity
-@Getter
-@Setter
-@NoArgsConstructor
 public class Person {
 
     @Id
@@ -20,4 +14,39 @@ public class Person {
     @Column(name = "last_name")
     private String lastName;
     private int age;
+
+    public Person() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
